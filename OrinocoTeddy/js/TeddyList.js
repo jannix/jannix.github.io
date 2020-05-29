8017718f17670b4ec5d089a0a91f88b300969e04
Jannix <jannix.martinez@gmail.com>
2020-05-29 13:05:55 +0200

    ori - lil clean

--- OrinocoTeddy/js/TeddyList.js
+++ OrinocoTeddy/js/TeddyList.js
@@ -14,30 +14,33 @@ function getTeddyList() {
 
 function initTeddyList(teddies) {
     let sectionList = document.getElementById('teddy-list');
-    var innerSection = '';
     for (let teddy of teddies)
     {
-        var colDiv = document.createElement('div');
-        colDiv.setAttribute('class', 'col-12 col-md-6 col-lg-3 my-3');
-        var cardDiv = document.createElement('div');
-        cardDiv.setAttribute('class', 'card h-100');
-        cardDiv.addEventListener('click', function() {
-            console.log('click and go to product', teddy['_id']);
-        });
-        var contentCard = "<img class=\"card-img-top\" src=\""+teddy['imageUrl']+"\" alt=\"Card image cap\">\n" +
-            "                <div class=\"card-body\" href=\"html/product\">\n" +
-            "                    <h5 class=\"card-title\">"+teddy['name']+"</h5>\n" +
-            "                    <h6 class=\"card-subtitle\">"+teddy['price'] / 100 +"</h6>\n" +
-            "                    <p class=\"card-text\">"+teddy['description']+"</p>\n" +
-            "                </div>";
-        cardDiv.innerHTML = contentCard;
-        colDiv.appendChild(cardDiv);
+        const colDiv = buildTeddyCard(teddy);
         sectionList.appendChild(colDiv);
     }
 }
 
 function buildTeddyCard(teddy) {
-    console.log(teddy);
+    var colDiv = document.createElement('div');
+    colDiv.setAttribute('class', 'col-12 col-md-6 col-lg-3 my-3');
+    var cardDiv = document.createElement('div');
+    cardDiv.setAttribute('class', 'card h-100');
+    cardDiv.addEventListener('click', function() {
+        console.log('click and go to product', teddy['_id']);
+        var url = './html/product.html?id=' + teddy['_id']
+
+        document.location.href = url;
+    });
+    var contentCard = "<img class=\"card-img-top\" src=\""+teddy['imageUrl']+"\" alt=\"Card image cap\">\n" +
+        "                <div class=\"card-body\" href=\"html/product\">\n" +
+        "                    <h5 class=\"card-title\">"+teddy['name']+"</h5>\n" +
+        "                    <h6 class=\"card-subtitle\">"+teddy['price'] / 100 +"</h6>\n" +
+        "                    <p class=\"card-text\">"+teddy['description']+"</p>\n" +
+        "                </div>";
+    cardDiv.innerHTML = contentCard;
+    colDiv.appendChild(cardDiv);
+    return colDiv;
 }
 
 
