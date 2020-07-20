fa759c23e5b7c80df08397b7d68e202885edd476
Jannix <jannix.martinez@gmail.com>
2020-07-20 05:54:54 +0200

    ori - add other teddy list in product page

--- OrinocoTeddy/js/TeddyDetails.js
+++ OrinocoTeddy/js/TeddyDetails.js
@@ -16,6 +16,12 @@ window.onload = function () {
         currentTeddy = teddy;
         initTeddyDetails(teddy);
     });
+
+    fetch("http://localhost:3000/api/teddies").then(res => {
+        return res.json();
+    }).then(teddies => {
+        initTeddyList(teddies);
+    });
 };
 
 function initTeddyDetails(teddy) {
@@ -68,3 +74,35 @@ function buildColorSelect(colors) {
         selectList.appendChild(option);
     }
 }
+function initTeddyList(teddies) {
+    let sectionList = document.getElementById('otherTeddies');
+    for (let teddy of teddies)
+    {
+        const colDiv = buildTeddyCard(teddy);
+        sectionList.appendChild(colDiv);
+    }
+}
+
+function buildTeddyCard(teddy) {
+    const colDiv = document.createElement('div');
+    colDiv.setAttribute('class', 'col-3 col-md-1 col-lg-2 my-3');
+    const cardDiv = document.createElement('div');
+    cardDiv.setAttribute('class', 'card h-100');
+    cardDiv.setAttribute('itemprop', 'itemListElement');
+    cardDiv.setAttribute('itemscope', '');
+    cardDiv.setAttribute('itemtype', 'http://schema.org/Product');
+    cardDiv.addEventListener('click', function() {
+        const url = './product.html?id=' + teddy['_id'];
+        document.location.href = url;
+    });
+    const contentCard = "<img alt=\"Photo of product\" itemprop=\"image\" class=\"card-img-top\" src=\"" + teddy['imageUrl'] + "\">\n" +
+        "                <div itemprop=\"url\" class=\"card-body\" href=\"html/product\">\n" +
+        "                    <h5 class=\"card-title\" itemprop=\"name\">" + teddy['name'] + "</h5>\n" +
+        "                    <h6 class=\"card-subtitle\" itemprop=\"price\">" + teddy['price'] / 100 + "€</h6>\n" +
+        "                </div>";
+    cardDiv.innerHTML = contentCard;
+    colDiv.appendChild(cardDiv);
+    return colDiv;
+}
+
+
