effbf01273032334f0615eda98c4ec1746b0d01e
Jannix <jannix.martinez@gmail.com>
2020-08-20 12:24:58 +0200

    sopeko - implement celebrate to validate form

--- SoPekocko/back/routes/user.routes.js
+++ SoPekocko/back/routes/user.routes.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const { celebrate, Joi, Segments } = require('celebrate');
 const router = express.Router();
 
 const userCtrl = require('../controllers/user.controller');
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+//router.post('/signup', userCtrl.signup);
+//router.post('/login', userCtrl.login);
+router.post('/signup', celebrate({
+        [Segments.BODY]: Joi.object().keys({
+            email: Joi.string().required(),
+            password: Joi.string().required(),
+        })
+    }), userCtrl.signup);
 
+router.post('/login', celebrate({
+        [Segments.BODY]: Joi.object().keys({
+            email: Joi.string().required(),
+            password: Joi.string().required(),
+        })
+    }), userCtrl.login);
 module.exports = router;
