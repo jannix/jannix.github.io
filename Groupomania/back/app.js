fd7684462c7169672e132c936b43a2987cecdea2
Jannix <jannix.martinez@gmail.com>
2020-10-07 15:47:21 +0200

    groupo - legend of the forgotten files

---
+++ Groupomania/back/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const db = require("./models");
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const helmet = require("helmet");
+const userRoutes = require('./routes/user.routes');
+//const secretConst = require('./constants/secret-constants');
+const path = require('path');
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/auth', userRoutes);
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to Groupomania Social Platfrom." });
+});
+
+app.use((req, res) => {
+    res.json({ message: 'Votre requête a bien été reçue !' });
+});
+
+db.sequelize.sync();/*{ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+});*/ //only for dev to drop tables
+
+module.exports = app;
