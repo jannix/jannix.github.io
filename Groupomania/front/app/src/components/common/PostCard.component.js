c5aa90f1161f0e76ba28d089859ff72e399eccba
Jannix <jannix.martinez@gmail.com>
2021-01-28 11:26:15 +0100

    groupoFR - fix votes event propagate

--- Groupomania/front/app/src/components/common/PostCard.component.js
+++ Groupomania/front/app/src/components/common/PostCard.component.js
@@ -57,12 +57,12 @@ export default class PostCard extends React.Component {
                 </div>
                 <div className="postcard-stats">
                     <span>
-                        <button className={'btn-vote'} onClick={() => this.sendVote(1)}>
+                        <button className={'btn-vote'} onClick={(event) => {this.sendVote(1);  event.stopPropagation();}}>
                             <div className="overlay"/>
                             <img src={window.location.origin + '/images/iconarrowup.png'} alt="Upvote" title="Upvote button"/>
                         </button>
                         {this.state.votes}
-                        <button className={'btn-vote'} onClick={() => this.sendVote(-1)}>
+                        <button className={'btn-vote'} onClick={(event) => {this.sendVote(-1);  event.stopPropagation();}}>
                             <div className="overlay"/>
                             <img src={window.location.origin + '/images/iconarrowdown.png'} alt="Downvote" title="Downvote button"/>
                         </button>
